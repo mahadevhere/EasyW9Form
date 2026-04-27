5674d214b7e7570d3efab7592a9d8768768dcc2d
i-mahadev <mahadeva3253@gmail.com>
2026-04-27 08:06:28 +0530

    commit

--- app/layout.js
+++ app/layout.js
@@ -233,6 +233,21 @@ export default function RootLayout({ children }) {
             })(window, document, "clarity", "script", "wi0zcg5yix");
           `}
         </Script>
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-PVSPJQ2XQW"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-PVSPJQ2XQW');
+          `}
+        </Script>
       </body>
     </html>
   );
